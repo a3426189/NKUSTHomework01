75d4b5eb8b5e5575381483a5fcbd09c8d8445898
1106105347 <1106105347@nkust.edu.tw>
2021-03-15 11:51:25 +0800

    Web json

---
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickType;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            using ( var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://apiservice.mol.gov.tw/OdService/download/A17000000J-030240-88Z");
+                var welcome = Welcome.FromJson(jsonString);
+                ViewData["Welcome"] = welcome;
+            }
+            
+        }
+    }
+}
