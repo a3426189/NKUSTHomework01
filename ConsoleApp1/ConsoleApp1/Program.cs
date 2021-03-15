1cbff79b47be8e3cf0c05feb8a3268deb3d8597f
1106105347 <1106105347@nkust.edu.tw>
2021-03-15 13:24:22 +0800

    Console.WriteLine(item.userId); Console.WriteLine(item.id); Console.WriteLine(item.title); Console.WriteLine(item.completed);

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-
 namespace ConsoleApp1
 {
     class Program
@@ -20,8 +20,15 @@ namespace ConsoleApp1
         {
             string response = await client.GetStringAsync("http://jsonplaceholder.typicode.com/todos");
 
-            Console.WriteLine(response);
-
+            //Console.WriteLine(response);
+            List<Todo> todo = JsonConvert.DeserializeObject<List<Todo>>(response);
+            foreach(var item in todo)
+            {
+                Console.WriteLine(item.userId);
+                Console.WriteLine(item.id);
+                Console.WriteLine(item.title);
+                Console.WriteLine(item.completed);
+            }
         }
         class Todo
         {
