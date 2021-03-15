4537813e9a6248f2c0915b2b72e2b9536d53be84
1106105347 <1106105347@nkust.edu.tw>
2021-03-15 13:13:14 +0800

    Console.WriteLine some thing

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetJsonData();
+
+        }
+
+        private async Task GetJsonData()
+        {
+            string response = await client.GetStringAsync("http://jsonplaceholder.typicode.com/todos");
+
+            Console.WriteLine(response);
+
+        }
+        class Todo
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public bool completed { get; set; }
+
+        }
+    }
+}
